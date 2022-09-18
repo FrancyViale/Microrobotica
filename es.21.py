f5b66c133e84d5bf187ef9e2eed3ee03d5919ae4
Francesco Viale <94169284+FrancyViale@users.noreply.github.com>
2022-09-18 17:18:39 +0200

    Add files via upload

---
+++ es.21.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+s="vocali"
+s=[c for c in s if (c != "a") ]
+s=[c for c in s if (c != "e") ]
+s=[c for c in s if (c != "i") ]
+s=[c for c in s if (c != "o") ]
+s=[c for c in s if (c != "u") ]
+s
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
